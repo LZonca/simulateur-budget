a25df96ec4157e854d66d7d7ea8b0c49bb06c422
evan7899 <evanferrer5@gmail.com>
2023-11-27 16:55:17 +0100

    changement tailwind final

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,10 @@
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-xl dark:bg-gray-800 sm:rounded-lg">
-
                 <x-welcome />
             </div>
         </div>
+        
     </div>
+    <x-footer/>
 </x-app-layout>
--- resources/views/resultats.blade.php
+++ resources/views/resultats.blade.php
@@ -1,3 +1,6 @@
 <x-app-layout>
-    <p>resultat</p>
+    <x-slot name="header">
+        <h1 class="text-2xl font-semibold mt-12">Résultats obtenu</h1>
+    </x-slot>
+    <x-footer/>
 </x-app-layout>

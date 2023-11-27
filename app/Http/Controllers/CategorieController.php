590f733308d40626a62850ffd0ca630ae0add042
LZonca <leo.zonca@business-school.academy>
2023-11-27 16:12:27 +0100

    ``` Add `simulations` relationship to `User` model.
    This commit adds a `simulations` relationship to the `User` model. This relationship allows us to access all of the simulations that a user has created.
    ``` (generated by blackbox.ai )

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categorie;
+use App\Http\Requests\StoreCategorieRequest;
+use App\Http\Requests\UpdateCategorieRequest;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategorieRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategorieRequest $request, Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Categorie $categorie)
+    {
+        //
+    }
+}

0c64a173c163a414cf20ce45866ccd4af7b4752e
Alexandre <122398566+Bonin-Alexandre@users.noreply.github.com>
2023-12-04 16:54:36 +0100

    commit front (Alex)

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,6 +1,9 @@
 
 <x-slot name="header">
-    <h1 class="text-2xl font-semibold mt-12">Bienvenue à la Mairie d'Arnas</h1>
+    <div class="flex items-center">
+        <img src="{{ asset('/img/arnas_img.png') }}" alt="Logo" class="mr-25 w-70 h-20">
+        <h1 class="text-2xl font-semibold ml-10">Bienvenue à la Mairie d'Arnas</h1>
+    </div>
 </x-slot>
 
 <main class="mx-10 bg-white ">
@@ -10,7 +13,7 @@
             développement de la ville. Nous sommes dédiés à la transparence, à l'efficacité et à la participation
             citoyenne.</p>
     </section>
-    <section class="mt-8">
+    <!--<section class="mt-8">
         <h2 class="text-2xl font-semibold mb-4">Nos Services</h2>
         <ul class="list-disc pl-6 text-gray-700">
             <li>Administration municipale</li>
@@ -18,7 +21,17 @@
             <li>Projets communautaires</li>
             <li>Événements locaux</li>
         </ul>
+    </section>-->
+
+    <section class="mt-8">
+        <h2 class="text-2xl font-semibold mb-4">But de ce simulateur</h2>
+        <p class="text-gray-700">Ce simulateur a pour but de permettre aux habitants de notre ville
+            de pouvoir gérer et planifier virtuellement le budget et les dépenses d'Arnas de manière
+            interactive et ludique. Ces outils offrent une alternative à la gestion des dépenses
+            traditionnelle, qui consiste à analyser et à catégoriser les dépenses dans des tableaux.
+        </p>
     </section>
+
     <section class="mt-8">
         <h2 class="text-2xl font-semibold mb-4">Engagement Communautaire</h2>
         <p class="text-gray-700 mb-10">Nous croyons en la participation active des citoyens dans la prise de décision. Le
--- resources/views/resultats.blade.php
+++ resources/views/resultats.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h1 class="text-2xl font-semibold mt-12">Résultats obtenu</h1>
+        <br><h1 class="text-2xl font-semibold m-4 mt-12 text-center">Résultats obtenus</h1>
     </x-slot>
     <x-footer/>
 </x-app-layout>

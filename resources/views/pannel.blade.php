20e740095fee00ed6361299d212535ae7ba42650
LZonca <leo.zonca@business-school.academy>
2023-12-04 16:02:22 +0100

    pannel update

--- resources/views/pannel.blade.php
+++ resources/views/pannel.blade.php
@@ -1,14 +1,12 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h1>Admin Panel</h1>
-    </x-slot>
 
-    <div>
-        <livewire:add-categorie />
-    </div>
     <x-slot name="header">
         <h1 class="mt-12 text-2xl font-semibold text-center">Admin</h1>
     </x-slot>
+    <div>
+        <livewire:add-categorie />
+    </div>
+
     <x-footer/>
 </x-app-layout>
 

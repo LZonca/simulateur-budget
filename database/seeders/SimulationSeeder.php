a22ae8d8aba52068461ae74df51eadfad9590502
LZonca <leo.zonca@business-school.academy>
2023-12-11 16:46:50 +0100

    sim seeder update

--- database/seeders/SimulationSeeder.php
+++ database/seeders/SimulationSeeder.php
@@ -11,42 +11,42 @@ class SimulationSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+
     public function run(): void
     {
         $s = new Simulation();
-        $s->simulations_nom	= "simulation1";
-        $s->simulations_date = fake()->date('Y-m-d H:i:s');
+        $s->simulations_nom = "simulation1";
+        $s->simulations_date = now();
         $s->simulations_resultats = "resultats1";
         $s->utilisateur_id = 1;
         $s->save();
 
         $s = new Simulation();
-        $s->simulations_nom	= "simulation2";
-        $s->simulations_date = fake()->date('Y-m-d H:i:s');
+        $s->simulations_nom = "simulation2";
+        $s->simulations_date = now();
         $s->simulations_resultats = "resultats2";
         $s->utilisateur_id = 1;
         $s->save();
 
         $s = new Simulation();
-        $s->simulations_nom	= "simulation3";
-        $s->simulations_date = fake()->date('Y-m-d H:i:s');
+        $s->simulations_nom = "simulation3";
+        $s->simulations_date = now();
         $s->simulations_resultats = "resultats3";
         $s->utilisateur_id = 2;
         $s->save();
 
         $s = new Simulation();
-        $s->simulations_nom	= "simulation4";
-        $s->simulations_date = fake()->date('Y-m-d H:i:s');
+        $s->simulations_nom = "simulation4";
+        $s->simulations_date = now();
         $s->simulations_resultats = "resultats4";
         $s->utilisateur_id = 2;
         $s->save();
 
         $s = new Simulation();
-        $s->simulations_nom	= "simulation5";
-        $s->simulations_date = fake()->date('Y-m-d H:i:s');
+        $s->simulations_nom = "simulation5";
+        $s->simulations_date = now();
         $s->simulations_resultats = "resultats5";
         $s->utilisateur_id = 2;
         $s->save();
-
     }
 }

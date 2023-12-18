e0fae9d9169308bce27fc4e035eaa3e00747ece7
Kiri <46458337+KiriMashi32@users.noreply.github.com>
2023-12-18 16:40:27 +0100

    jsp

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Simulation;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
@@ -29,11 +33,5 @@ class Controller extends BaseController
 
     public function pannel(){
         return view('pannel');
-    }
-
-    public function superAdminPannel(){
-        $adminRole = Role::where('name', 'admin')->first();
-        $users = User::all();
-        return view('super-admin-pannel', compact('users'));
     }
 }

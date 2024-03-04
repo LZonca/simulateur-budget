93d46af876e01044b15ce25b45505d76426277c0
erwanngat <ewnran69@yahoo.fr>
2024-03-04 14:24:08 +0100

    fix seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.

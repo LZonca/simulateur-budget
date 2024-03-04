79656141b97e7ab0b117d7e9231e1721cc19e1e7
LZonca <leo.zonca@business-school.academy>
2024-03-04 16:55:04 +0100

    Revert "Fix MaryUI"
    This reverts commit 932d71d68187333622b106e9b06248545ea65b55.

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,26 +1,27 @@
 <!--<div class="flex items-center bg-green-500">
-    <img src="{{ asset('/img/arnas_img.png') }}" alt="Logo" class="h-20 mr-25 w-70">
-    <h1 class="ml-10 text-2xl font-semibold">Bienvenue à la Mairie d'Arnas</h1>
+    <img src="{{ asset('/img/arnas_img.png') }}" alt="Logo" class="mr-25 w-70 h-20">
+    <h1 class="text-2xl font-semibold ml-10">Bienvenue à la Mairie d'Arnas</h1>
 </div>-->
 
 <main class="mx-10 bg-white ">
     <section>
-        <h2 class="mb-4 text-2xl font-semibold">Présentation de la Mairie</h2>
+        <h2 class="text-2xl font-semibold mb-4">Présentation de la Mairie</h2>
         <p class="text-gray-700">La mairie d'Arnas est au service de la communauté pour assurer le bien-être et le
             développement de la ville. Nous sommes dédiés à la transparence, à l'efficacité et à la participation
             citoyenne.</p>
     </section>
     <!--<section class="mt-8">
-        <h2 class="mb-4 text-2xl font-semibold">Nos Services</h2>
-        <ul class="pl-6 text-gray-700 list-disc">
+        <h2 class="text-2xl font-semibold mb-4">Nos Services</h2>
+        <ul class="list-disc pl-6 text-gray-700">
             <li>Administration municipale</li>
             <li>Gestion des services publics</li>
             <li>Projets communautaires</li>
             <li>Événements locaux</li>
         </ul>
     </section>-->
+
     <section class="mt-8">
-        <h2 class="mb-4 text-2xl font-semibold">But de ce simulateur</h2>
+        <h2 class="text-2xl font-semibold mb-4">But de ce simulateur</h2>
         <p class="text-gray-700">Ce simulateur a pour but de permettre aux habitants de notre ville
             de pouvoir gérer et planifier virtuellement le budget et les dépenses d'Arnas de manière
             interactive et ludique. Ces outils offrent une alternative à la gestion des dépenses
@@ -29,9 +30,8 @@
     </section>
 
     <section class="mt-8">
-        <h2 class="mb-4 text-2xl font-semibold">Engagement Communautaire</h2>
-        <p class="mb-10 text-gray-700">Nous croyons en la participation active des citoyens dans la prise de décision.
-            Le
+        <h2 class="text-2xl font-semibold mb-4">Engagement Communautaire</h2>
+        <p class="text-gray-700 mb-10">Nous croyons en la participation active des citoyens dans la prise de décision. Le
             simulateur de budget est l'une de nos initiatives pour permettre aux habitants de proposer et de voter
             pour des projets qui façonnent l'avenir de notre ville.</p>
     </section>

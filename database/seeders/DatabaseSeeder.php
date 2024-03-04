9431d8105aefed2fdf7eecd4be9b52fb4192b297
erwanngat <ewnran69@yahoo.fr>
2024-03-04 14:24:08 +0100

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,8 +23,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             PermissionSeeder::class,
-            SimulationSeeder::class,
             UserSeeder::class,
+            SimulationSeeder::class,
         ]);
     }
 }

c37dffafbb795c53fd103157a526e2fcee8a0872
erwanngat <ewnran69@yahoo.fr>
2024-03-04 14:57:22 +0100

    fix navbar

--- resources/views/resultats.blade.php
+++ resources/views/resultats.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h1 class="text-2xl font-semibold m-4 mt-12 text-center">Résultats obtenus</h1>
+        <br/><h1 class="text-2xl font-semibold m-4 mt-12 text-center">Résultats obtenus</h1>
     </x-slot>
 
     <div class="py-5">

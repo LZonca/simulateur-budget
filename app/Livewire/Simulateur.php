3ad1f628407e6c41db30b5d1cf0a3706df1a880a
Kiri <46458337+KiriMashi32@users.noreply.github.com>
2024-03-11 15:25:09 +0100

    debut bar

--- app/Livewire/Simulateur.php
+++ app/Livewire/Simulateur.php
@@ -6,26 +6,8 @@ use Livewire\Component;
 
 class Simulateur extends Component
 {
-    public array $simulateurChart;
+    public int $level = 10;
 
-    public function mount(){
-       $this->simulateurChart = [
-            'type' => 'pie',
-            'data' => [
-                'labels' => $this->getCategories(),
-                'datasets' => [
-                    [
-                        'label' => '# of Votes',
-                        'data' => [12, 19, 3],
-                    ]
-                ]
-            ]
-        ];
-    }
-
-    public function getCategories(){
-        return ['Mary', 'Joe', 'Ana'];
-    }
     public function render()
     {
         return view('livewire.simulateur');
--- resources/views/livewire/simulateur.blade.php
+++ resources/views/livewire/simulateur.blade.php
@@ -1,3 +1,3 @@
 <div>
-    <x-chart wire:model="simulateurChart" />
+    <x-range wire:model.debounce.500ms="level" label="Select a level" hint="Greater than 10." />
 </div>

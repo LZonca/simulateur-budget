d3088e372693f126fe30a852f58e713293013448
erwanngat <ewnran69@yahoo.fr>
2024-04-08 14:59:16 +0200

    seeder montant

--- database/seeders/SousCategorieSeeder.php
+++ database/seeders/SousCategorieSeeder.php
@@ -16,181 +16,217 @@ class SousCategorieSeeder extends Seeder
         $s = new SousCategorie();
         $s->sous_categorie_nom = "charges de personnel";
         $s->categorie_id = 1;
+        $s->montant = 0;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "opérations ordre";
         $s->categorie_id = 1;
+        $s->montant = 0;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "amortissement";
         $s->categorie_id = 1;
+        $s->montant = 185877;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Administration générale";
         $s->categorie_id = 2;
+        $s->montant = 582483;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Communication";
         $s->categorie_id = 2;
+        $s->montant = 34900;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Fêtes et cérémonies";
         $s->categorie_id = 2;
+        $s->montant = 41273;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Subventions";
         $s->categorie_id = 2;
+        $s->montant = 19602;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Cimetière";
         $s->categorie_id = 2;
+        $s->montant = 569;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Police municipale";
         $s->categorie_id = 3;
+        $s->montant = 43728;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "contribution incendie";
         $s->categorie_id = 3;
+        $s->montant = 2405;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Charges de personnel";
         $s->categorie_id = 4;
+        $s->montant = 318567;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Restauration scolaire";
         $s->categorie_id = 4;
+        $s->montant = 87464;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Fournitures scolaires";
         $s->categorie_id = 4;
+        $s->montant = 23896;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Fluides et produits entretien";
         $s->categorie_id = 4;
+        $s->montant = 58478;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Matériel (blouses/pharmacie/dictionnaires/pronote/petit matériel de protection)";
         $s->categorie_id = 4;
+        $s->montant = 2856;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Photocopieurs";
         $s->categorie_id = 4;
+        $s->montant = 3068;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Transports bus + tennis + animations";
         $s->categorie_id = 4;
+        $s->montant = 5260;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Bâtiments";
         $s->categorie_id = 4;
+        $s->montant = 17154;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Location portakabin";
         $s->categorie_id = 4;
+        $s->montant = 48842;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Participation autres écoles";
         $s->categorie_id = 4;
+        $s->montant = 242601;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Frais emprunt";
         $s->categorie_id = 4;
+        $s->montant = 72877;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Médiathèque";
         $s->categorie_id = 5;
+        $s->montant = 52803;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Activité du service";
         $s->categorie_id = 5;
+        $s->montant = 14128;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Bâtiments";
         $s->categorie_id = 6;
+        $s->montant = 8767;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Fluides";
         $s->categorie_id = 6;
+        $s->montant = 11176;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Jeunesse (centres aérés extérieurs et CMJ)";
         $s->categorie_id = 6;
+        $s->montant = 3370;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Charges de personnel (75% Marielle et 75 % Alexis)";
         $s->categorie_id = 6;
+        $s->montant = 46878;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Chèque déjeuner et cotisation CNAS";
         $s->categorie_id = 7;
+        $s->montant = 21590;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Centre de loisirs et RPE";
         $s->categorie_id = 8;
+        $s->montant = 243020;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Fluides";
         $s->categorie_id = 8;
+        $s->montant = 1234;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Bâtiments";
         $s->categorie_id = 8;
+        $s->montant = 5521;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Charges de personnel";
         $s->categorie_id = 10;
+        $s->montant = 311693;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Fluides";
         $s->categorie_id = 10;
+        $s->montant = 29487;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Charges SYDER";
         $s->categorie_id = 10;
+        $s->montant = 601041;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "Services et aménagements urbains";
         $s->categorie_id = 10;
+        $s->montant = 125204;
         $s->save();
 
         $s = new SousCategorie();
         $s->sous_categorie_nom = "conso marché";
         $s->categorie_id = 11;
+        $s->montant = 9076;
         $s->save();
 
 
--- database/seeders/SousSousCategorieSeeder.php
+++ database/seeders/SousSousCategorieSeeder.php
@@ -16,86 +16,103 @@ class SousSousCategorieSeeder extends Seeder
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "charges de personnel";
         $s->sous_categorie_id = 4;
+        $s->montant = 208962;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "Conventions ou prestations de services ponctuelles";
         $s->sous_categorie_id = 4;
+        $s->montant = 66340;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "fluides, produits, maintenance, etc";
         $s->sous_categorie_id = 4;
+        $s->montant = 49976;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "téléphonie et affranchissement";
         $s->sous_categorie_id = 4;
+        $s->montant = 10788;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "assurance";
         $s->sous_categorie_id = 4;
+        $s->montant = 30262;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "indemnités élus";
         $s->sous_categorie_id = 4;
+        $s->montant = 111647;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "CCAS";
         $s->sous_categorie_id = 4;
+        $s->montant = 11600;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "FPIC";
         $s->sous_categorie_id = 4;
+        $s->montant = 10780;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "Pénalités loi SRU";
         $s->sous_categorie_id = 4;
+        $s->montant = 42500;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "taxes";
         $s->sous_categorie_id = 4;
+        $s->montant = 9628;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "charges de personnel";
         $s->sous_categorie_id = 9;
+        $s->montant = 41296;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "Vêtements et matériels professionnels";
         $s->sous_categorie_id = 9;
+        $s->montant = 433;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "Logiciels";
         $s->sous_categorie_id = 9;
+        $s->montant = 80;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "Véhicule";
         $s->sous_categorie_id = 9;
+        $s->montant = 311;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "Activité du service (mises en fourrière/stérilisation chats errants/adhésion Ville Prudente)";
         $s->sous_categorie_id = 9;
+        $s->montant = 1608;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "Charges de personnel";
         $s->sous_categorie_id = 22;
+        $s->montant = 37910;
         $s->save();
 
         $s = new SousSousCategorie();
         $s->sous_sous_categorie_nom = "Fluides et bâtiment";
         $s->sous_categorie_id = 22;
+        $s->montant = 14893;
         $s->save();
     }
 }

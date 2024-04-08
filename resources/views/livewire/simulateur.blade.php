932926576f9552f5b93a0e16492bcf5813f71df1
Kiri <46458337+KiriMashi32@users.noreply.github.com>
2024-04-08 15:22:40 +0200

    eeeeeeeeeeeeelp

--- resources/views/livewire/simulateur.blade.php
+++ resources/views/livewire/simulateur.blade.php
@@ -7,18 +7,18 @@
         <div class="border p-4 rounded-lg">
             <label for="level{{$categorie->id}}">{{$categorie->categorie_nom}} :</label>
             <div class="flex justify-between items-center mt-2">
-                <button wire:click="adjustLevel{{$categorie->id}}(-10)" class="px-2 py-1 bg-blue-500 text-white rounded-md">-10%</button>
-                <input type="number" id="level{{$categorie->id}}" wire:model="level{{$categorie->id}}" class="value-input w-2/3 px-2 py-1 border rounded-md">
-                <button wire:click="adjustLevel{{$categorie->id}}(10)" class="px-2 py-1 bg-blue-500 text-white rounded-md">+10%</button>
+                <button wire:click="adjustLevel({{$categorie->id}},-10)" class="px-2 py-1 bg-blue-500 text-white rounded-md">-10%</button>
+                <input type="number" id="level{{$categorie->id}}" wire:model="level{{$categorie->id}}" class="value-input w-2/3 px-2 py-1 border rounded-md" placeholder="{{$categorie->montant}}">
+                <button wire:click="adjustLevel({{$categorie->id}}, 10)" class="px-2 py-1 bg-blue-500 text-white rounded-md">+10%</button>
             </div>
         </div>
 
-        @foreach($sousCategories->where('categorie_id', $categorie->id) as $sousCategorie)
+        {{-- @foreach($sousCategories->where('categorie_id', $categorie->id) as $sousCategorie)
             <div class="border p-4 rounded-lg p-2">
                 <label for="level{{$sousCategorie->id}}">{{$sousCategorie->sous_categorie_nom}} :</label>
                 <div class="flex justify-between items-center mt-2">
                     <button wire:click="adjustLevel{{$sousCategorie->id}}(-10)" class="px-2 py-1 bg-blue-500 text-white rounded-md">-10%</button>
-                    <input type="number" id="level{{$sousCategorie->id}}" wire:model="level{{$sousCategorie->id}}" class="value-input w-2/3 px-2 py-1 border rounded-md">
+                    <input type="number" id="level{{$sousCategorie->id}}" wire:model="level{{$sousCategorie->id}}" class="value-input w-2/3 px-2 py-1 border rounded-md" placeholder="{{$sousCategorie->montant}}">
                     <button wire:click="adjustLevel{{$sousCategorie->id}}(10)" class="px-2 py-1 bg-blue-500 text-white rounded-md">+10%</button>
                 </div>
             </div>
@@ -28,12 +28,12 @@
                     <label for="level{{$sousSousCategorie->id}}">{{$sousSousCategorie->sous_sous_categorie_nom}} :</label>
                     <div class="flex justify-between items-center mt-2">
                         <button wire:click="adjustLevel{{$sousSousCategorie->id}}(-10)" class="px-2 py-1 bg-blue-500 text-white rounded-md">-10%</button>
-                        <input type="number" id="level{{$sousSousCategorie->id}}" wire:model="level{{$sousSousCategorie->id}}" class="value-input w-2/3 px-2 py-1 border rounded-md">
+                        <input type="number" id="level{{$sousSousCategorie->id}}" wire:model="level{{$sousSousCategorie->id}}" class="value-input w-2/3 px-2 py-1 border rounded-md" placeholder="{{$sousSousCategorie->montant}}">
                         <button wire:click="adjustLevel{{$sousSousCategorie->id}}(10)" class="px-2 py-1 bg-blue-500 text-white rounded-md">+10%</button>
                     </div>
                 </div>
             @endforeach
-        @endforeach
+        @endforeach --}}
     @endforeach
 
     <!-- Catégorie D (si nécessaire) -->
